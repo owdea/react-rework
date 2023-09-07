3a6a0959bafaaef40809abd61900800f91c12017
Owdea <ondrejpel@seznam.cz>
2023-09-07 17:38:33 +0200

    První commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='App'>
+      <Navbar />
+      <div className='content'>
+        <Home />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+    return(
+        <nav className="navbar">
+            <h1>Mé portfolium</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
