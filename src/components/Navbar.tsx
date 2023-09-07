51deed2e50ace052e2efc464caf866a6060eb6ff
Owdea <ondrejpel@seznam.cz>
2023-09-07 17:52:18 +0200

    Buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,8 @@ function Navbar() {
             <h1>Mé portfolium</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/2nd project">2nd project</a>
+                <a href="/calculator">Calculator</a>
             </div>
         </nav>
     );
